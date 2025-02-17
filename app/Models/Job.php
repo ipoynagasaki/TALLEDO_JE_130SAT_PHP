965614a68478241cfacd2b1d7f3ad4b617d5e996
ipoy420 <jeffersontalledo1@gmail.com>
2025-02-17 23:17:45 +0800

    0217_8_8-introduction-to-migration_lab

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,10 +2,38 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
-class Job extends Model {
-    protected $table = 'job_listings';
+class Job {
+    public static function all(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$10,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$40,000'
+            ]
+        ];
+    }
 
-    protected $fillable = ['title', 'salary'];
+    public static function find(int $id): array
+    {
+        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+        if (! $job) {
+            abort(404);
+        }
+
+        return $job;
+    }
 }
